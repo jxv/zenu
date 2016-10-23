a80429aad03441bc1b883cca0978755d90250b89
jxv <joevargas92@gmail.com>
2016-10-23 01:44:17 -0700

    some refactors

--- main.cc
+++ main.cc
@@ -6,6 +6,8 @@
 #include <cstdlib>
 #include <cassert>
 
+#include <ft2build.h>
+
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
@@ -15,6 +17,8 @@
 
 #include "kolor.vert"
 #include "kolor.frag"
+
+///
 
 const float cube_positions[3*24] = {
   -0.5f, -0.5f, -0.5f,
@@ -43,6 +47,8 @@ const float cube_positions[3*24] = {
   0.5f,  0.5f, -0.5f,
 };
 
+const size_t cube_positions_size = 3 * 24;
+
 const unsigned short int cube_indices[36] = {
   0, 2, 1,
   0, 3, 2, 
@@ -58,108 +64,79 @@ const unsigned short int cube_indices[36] = {
   20, 22, 21
 };
 
-GLuint cube_vbo;
-GLuint cube_ibo;
+const size_t cube_indices_size = 36;
 
-struct shader_kolor {
+///
+
+typedef GLuint shader_id;
+typedef GLuint program_id;
+typedef GLuint vbo;
+typedef GLuint ibo;
+
+struct mesh {
+  GLuint vbo;
+  GLuint ibo;
+};
+
+struct source {
+  const GLchar *content;
+  GLint length;
+};
+
+struct shaders {
+  shader_id vert;
+  shader_id frag;
+};
+
+struct mesh_kolor {
+  GLuint vbo;
+  GLuint ibo;
+  int start_index;
+  int end_index;
+  size_t stride;
+};
+
+struct uniforms_kolor {
+  glm::mat4 &mvp;
+  glm::vec3 &kolor;
+};
+
+struct ref_kolor {
+  GLuint program;
+  GLint a_position;
+  GLint u_mvp;
+  GLint u_kolor;
+};
+
+///
+
+struct {
     GLuint program;
     GLint a_position;
     GLint u_mvp;
     GLint u_kolor;
-};
+} shader_kolor;
 
-shader_kolor shader_kolor;
+mesh cube;
 
-GLuint make_shader(const char *src, GLenum type, int src_len) {
-  GLuint shader = glCreateShader(type);
-  glShaderSource(shader, 1, &src, src_len == 0 ? NULL : &src_len);
-  glCompileShader(shader);
-  GLint status;
-  glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-  if (status == GL_FALSE) {
-    GLint len;
-    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
-    GLchar *info = (GLchar*)calloc(len, sizeof(GLchar));
-    glGetShaderInfoLog(shader, len, NULL, info);
-    fprintf(stderr, "glCompileShader failed:\n%s\n", info);
-    free(info);
-    return 0;
-  }
-  return shader;
-}
-
-GLuint make_program(GLuint vert_shader, GLuint frag_shader) {
-  GLuint program = glCreateProgram();
-  glAttachShader(program, vert_shader);
-  glAttachShader(program, frag_shader);
-  glLinkProgram(program);
-  GLint status;
-  glGetProgramiv(program, GL_LINK_STATUS, &status);
-  if (status == false) {
-    int len;
-    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &len);
-    GLchar *info = (GLchar*)calloc(len, sizeof(GLchar));
-    glGetProgramInfoLog(program, len, NULL, info);
-    fprintf(stderr, "glLinkProgram failed: %s\n", info);
-    free(info);
-  }
-  glDetachShader(program, vert_shader);
-  glDetachShader(program, frag_shader);
-  glDeleteShader(vert_shader);
-  glDeleteShader(frag_shader);
-  return program;
-}
-
-void setup_kolor_shader() {
-  const GLuint vert = make_shader(kolor_vert_src, GL_VERTEX_SHADER, strlen(kolor_vert_src));
-  const GLuint frag = make_shader(kolor_frag_src, GL_FRAGMENT_SHADER, strlen(kolor_frag_src));
-  shader_kolor.program = make_program(vert, frag);
-  glUseProgram(shader_kolor.program);
-
-  shader_kolor.a_position = glGetAttribLocation(shader_kolor.program, "a_position");
-  shader_kolor.u_mvp = glGetUniformLocation(shader_kolor.program, "u_mvp");
-  shader_kolor.u_kolor = glGetUniformLocation(shader_kolor.program, "u_kolor");
-
-  glGenBuffers(1, &cube_vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, cube_vbo);
-  glBufferData(GL_ARRAY_BUFFER, 24 * 3 * sizeof(float), cube_positions, GL_STATIC_DRAW);
-
-  glGenBuffers(1, &cube_ibo);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cube_ibo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 36 * 3 * sizeof(unsigned short int), cube_indices, GL_STATIC_DRAW);
-}
-
-void render_kolor_cube(const glm::mat4 &mvp, glm::vec3 &kolor) {
-  const int start_idx = 0;
-  const int end_idx = 36 - 1;
-
-  glUseProgram(shader_kolor.program);
-  // Set uniforms
-  glUniformMatrix4fv(shader_kolor.u_mvp, 1, GL_FALSE, (float*)&mvp);
-  glUniform3fv(shader_kolor.u_kolor, 1, (float*)&kolor);
-  // Set texture
-  glBindBuffer(GL_ARRAY_BUFFER, cube_vbo);
-  glEnableVertexAttribArray(shader_kolor.a_position);
-  glVertexAttribPointer(shader_kolor.a_position, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cube_ibo);
-  glDrawElements(GL_TRIANGLES, 1 + end_idx - start_idx, GL_UNSIGNED_SHORT, (void*)(sizeof(GLushort) * start_idx));
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-  glDisableVertexAttribArray(shader_kolor.a_position);
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-}
-
+///
 
 class renderer;
 class controller;
 class timer;
+class shader_manager;
+class program_manager;
+class loader;
+
+///
 
 class app {
 public:
-  app(renderer &renderer, controller &controller, timer &timer)
+  app(renderer &renderer, controller &controller, timer &timer, loader &loader)
   : renderer_(renderer)
   , controller_(controller)
   , timer_(timer)
+  , loader_(loader)
   {};
   void init();
   void quit();
@@ -168,6 +145,7 @@ private:
   renderer &renderer_;
   controller &controller_;
   timer &timer_;
+  loader &loader_;
 };
 
 class renderer {
@@ -183,7 +161,9 @@ private:
 
 class controller {
 public:
-  controller() : quit_(false) {};
+  controller()
+  : quit_(false)
+  {};
   void poll();
   bool quit();
 private:
@@ -195,22 +175,44 @@ public:
   void delay();
 };
 
-glm::mat4 model_view(float translate, glm::vec2 const & rotate) {
-  glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -translate));
-  view = glm::rotate(view, rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
-  view = glm::rotate(view, rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
-  glm::mat4 model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
-  return view * model;
-}
+class shader_manager {
+public:
+  shader_id create(const source &source, GLenum type);
+  void destroy(shader_id shader_id);
+};
 
-glm::mat4 camera(float translate, glm::vec2 const & rotate) {
-  glm::mat4 projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.f);
-  glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -translate));
-  view = glm::rotate(view, rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
-  view = glm::rotate(view, rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
-  glm::mat4 model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
-  return projection * view * model;
-}
+class program_manager {
+public:
+  program_id create(shader_id vert_shader, shader_id frag_shader);
+  void destroy(program_id program_id);
+};
+
+class mesh_manager {
+public:
+  mesh create(GLsizeiptr vertex_size, const GLvoid *vertices, GLsizeiptr index_size, const GLvoid *indices);
+};
+
+class loader {
+public:
+  loader(program_manager &program_manager, shader_manager &shader_manager, mesh_manager &mesh_manager)
+  : shader_manager_(shader_manager)
+  , program_manager_(program_manager)
+  , mesh_manager_(mesh_manager)
+  {}
+  void load();
+private:
+  shader_manager shader_manager_;
+  program_manager program_manager_;
+  mesh_manager mesh_manager_;
+};
+
+//
+
+program_id make_program(shader_manager &shader_manager, program_manager &program_manager, const source &vert_source, const source &frag_source);
+void render_kolor(const ref_kolor &ref, const mesh_kolor &mesh, const uniforms_kolor &uni);
+glm::mat4 camera(float translate, glm::vec2 const & rotate);
+
+//
 
 void controller::poll() {
   SDL_Event event;
@@ -230,7 +232,7 @@ bool controller::quit() {
 
 int app::main() {
   renderer_.init();
-  setup_kolor_shader();
+  loader_.load();
   while (!controller_.quit()) {
     controller_.poll();
     renderer_.clear();
@@ -241,7 +243,28 @@ int app::main() {
       rotate.y += 0.03;
       glm::mat4 mvp = camera(translate, rotate);
       glm::vec3 kolor(1.f,1.f,1.f);
-      render_kolor_cube(mvp, kolor);
+
+      const ref_kolor ref = {
+        .program = shader_kolor.program,
+        .a_position = shader_kolor.a_position,
+        .u_mvp = shader_kolor.u_mvp,
+        .u_kolor = shader_kolor.u_kolor,
+      };
+
+      const mesh_kolor mesh = {
+        .vbo = cube.vbo,
+        .ibo = cube.ibo,
+        .start_index = 0,
+        .end_index = 36 - 1,
+        .stride = 3 * sizeof(float),
+      };
+
+      const uniforms_kolor uni = {
+        .mvp = mvp,
+        .kolor = kolor,
+      };
+
+      render_kolor(ref, mesh, uni);
     }
     renderer_.render();
     timer_.delay();
@@ -319,11 +342,128 @@ void timer::delay() {
   SDL_Delay(16);
 }
 
+shader_id shader_manager::create(const source &source, GLenum type) {
+  shader_id shader_id = glCreateShader(type);
+  glShaderSource(shader_id, 1, &source.content, source.length == 0 ? NULL : &source.length);
+  glCompileShader(shader_id);
+  GLint status;
+  glGetShaderiv(shader_id, GL_COMPILE_STATUS, &status);
+  if (status == GL_FALSE) {
+    GLint len;
+    glGetShaderiv(shader_id, GL_INFO_LOG_LENGTH, &len);
+    GLchar *info = (GLchar*)calloc(len, sizeof(GLchar));
+    glGetShaderInfoLog(shader_id, len, NULL, info);
+    fprintf(stderr, "glCompileShader failed:\n%s\n", info);
+    free(info);
+    return 0;
+  }
+  return shader_id;
+}
+
+void shader_manager::destroy(shader_id shader_id) {
+  glDeleteShader(shader_id);
+}
+
+program_id program_manager::create(shader_id vert_shader, shader_id frag_shader) {
+  program_id program_id = glCreateProgram();
+  glAttachShader(program_id, vert_shader);
+  glAttachShader(program_id, frag_shader);
+  glLinkProgram(program_id);
+  GLint status;
+  glGetProgramiv(program_id, GL_LINK_STATUS, &status);
+  if (status == false) {
+    int len;
+    glGetProgramiv(program_id, GL_INFO_LOG_LENGTH, &len);
+    GLchar *info = (GLchar*)calloc(len, sizeof(GLchar));
+    glGetProgramInfoLog(program_id, len, NULL, info);
+    fprintf(stderr, "glLinkProgram failed: %s\n", info);
+    free(info);
+  }
+  glDetachShader(program_id, vert_shader);
+  glDetachShader(program_id, frag_shader);
+  return program_id;
+}
+
+void program_manager::destroy(program_id program_id) {
+  glDeleteProgram(program_id);
+}
+
+mesh mesh_manager::create(GLsizeiptr vertex_size, const GLvoid *vertices, GLsizeiptr index_size, const GLvoid *indices) {
+  mesh mesh;
+  glGenBuffers(1, &mesh.vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, mesh.vbo);
+  glBufferData(GL_ARRAY_BUFFER, vertex_size, vertices, GL_STATIC_DRAW);
+
+  glGenBuffers(1, &mesh.ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.ibo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_size, indices, GL_STATIC_DRAW);
+  return mesh;
+}
+
+void loader::load() {
+  const source vertex_source = {
+    .content = kolor_vert_src,
+    .length = (GLint)strlen(kolor_vert_src),
+  };
+  const source fragment_source = {
+    .content = kolor_frag_src,
+    .length = (GLint)strlen(kolor_frag_src),
+  };
+  const program_id program_id = make_program(shader_manager_, program_manager_, vertex_source, fragment_source);
+  glUseProgram(program_id);
+
+  shader_kolor.program = program_id;
+  shader_kolor.a_position = glGetAttribLocation(shader_kolor.program, "a_position");
+  shader_kolor.u_mvp = glGetUniformLocation(shader_kolor.program, "u_mvp");
+  shader_kolor.u_kolor = glGetUniformLocation(shader_kolor.program, "u_kolor");
+
+  cube = mesh_manager_.create(cube_positions_size * sizeof(float), cube_positions, cube_indices_size * sizeof(unsigned short int), cube_indices);
+}
 
 int main() {
   renderer renderer;
   controller controller;
   timer timer;
-  app app(renderer, controller, timer);
+  program_manager program_manager;
+  shader_manager shader_manager;
+  mesh_manager mesh_manager;
+  loader loader(program_manager, shader_manager, mesh_manager);
+  app app(renderer, controller, timer, loader);
   return app.main();
+}
+
+void render_kolor(const ref_kolor &ref, const mesh_kolor &mesh, const uniforms_kolor &uni) {
+  glUseProgram(ref.program);
+  
+  glUniformMatrix4fv(ref.u_mvp, 1, GL_FALSE, (float*)&uni.mvp);
+  glUniform3fv(ref.u_kolor, 1, (float*)&uni.kolor);
+
+  glBindBuffer(GL_ARRAY_BUFFER, mesh.vbo);
+  glEnableVertexAttribArray(ref.a_position);
+  glVertexAttribPointer(ref.a_position, 3, GL_FLOAT, GL_FALSE, mesh.stride, 0);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.ibo);
+  glDrawElements(GL_TRIANGLES, 1 + mesh.end_index - mesh.start_index, GL_UNSIGNED_SHORT, (void*)(sizeof(GLushort) * mesh.start_index));
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+  glDisableVertexAttribArray(ref.a_position);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+program_id make_program(shader_manager &shader_manager, program_manager &program_manager, const source &vert_source, const source &frag_source) {
+  const shader_id vert = shader_manager.create(vert_source, GL_VERTEX_SHADER);
+  const shader_id frag = shader_manager.create(frag_source, GL_FRAGMENT_SHADER);
+  const program_id program_id = program_manager.create(vert, frag);
+  shader_manager.destroy(vert);
+  shader_manager.destroy(frag);
+  return program_id;
+}
+
+
+glm::mat4 camera(float translate, glm::vec2 const & rotate) {
+  glm::mat4 projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.f);
+  glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -translate));
+  view = glm::rotate(view, rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
+  view = glm::rotate(view, rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
+  glm::mat4 model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
+  return projection * view * model;
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 all:
-	g++ main.cc -o zenu -std=c++11 `sdl2-config --cflags --libs` -lstdc++ -lm -lGLESv2 -D_GNU_SOURCE -ffast-math
+	g++ main.cc -o zenu -std=c++11 `sdl2-config --cflags --libs` `pkg-config --libs --cflags freetype2` -lstdc++ -lm -lGLESv2 -D_GNU_SOURCE -ffast-math
 
 gcw0:
-	/opt/gcw0-toolchain/usr/bin/mipsel-linux-g++ main.cc -o zenu -std=c++11 -I/opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/include/SDL2 -D_REENTRANT -L/opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/lib -lstdc++ -lm -lSDL2 -lpthread -lGLESv2 -D_GNU_SOURCE -ffast-math -DGCW0
+	/opt/gcw0-toolchain/usr/bin/mipsel-linux-g++ main.cc -o zenu -std=c++11 -I/opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/include/SDL2 -D_REENTRANT -L/opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/lib `/opt/gcw0-toolchain/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/bin/freetype-config --prefix --libs --cflags` -lstdc++ -lm -lSDL2 -lpthread -lGLESv2 -D_GNU_SOURCE -ffast-math -DGCW0
